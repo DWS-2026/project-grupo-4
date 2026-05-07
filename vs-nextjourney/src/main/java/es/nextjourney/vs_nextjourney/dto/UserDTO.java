fb23c83a13f3b16447a82a21453393e1755a0406
hurg05 <h.rus.2023@alumnos.urjc.es>
2026-05-07 20:00:49 +0200

    Fix password visible in endpoint

--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/dto/UserDTO.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/dto/UserDTO.java
@@ -2,10 +2,13 @@ package es.nextjourney.vs_nextjourney.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UserDTO(
     Long id,
     String username,
     String email,
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     String password,
     ImageDTO image,
     List<String> roles
